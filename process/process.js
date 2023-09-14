0ad86563912d4819d5bcd68caf14653527116630
Alex <kolo-ky@yandex.ru>
2023-09-15 00:06:57 +0500

    spawn

--- process/process.js
+++ process/process.js
@@ -12,3 +12,15 @@ const childProcess = exec('dir', (error, stdout, stderr) => {
 childProcess.on('exit', code => {
 	console.log(`out code ${code}`);
 });
+
+const { spawn } = require('child_process');
+
+const childSpawnProcess = spawn('dir');
+
+childSpawnProcess.stdout.on('data', data => {
+	console.log(`stdout ${data}`);
+});
+
+childSpawnProcess.stderr.on('data', data => {
+	console.error(`stderr ${data.message}`);
+});
